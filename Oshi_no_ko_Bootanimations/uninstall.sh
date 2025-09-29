fb3ca0154fde7beabf44d768841299b942762624
アントネッロ <87477585+Anto426@users.noreply.github.com>
2025-09-29 11:07:47 +0200

    Delete Oshi_no_ko_Bootanimations directory

--- Oshi_no_ko_Bootanimations/uninstall.sh
+++
@@ -1,3 +0,0 @@
-  cp "/data/adb/modules/Oshi_no_ko_bootanimation-lite/backup/bootanimation.zip" "/product/media/" >/dev/null 2>&1
-  rm -rf /data/adb/modules/Oshi_no_ko_bootanimation-lite/*
-  rm -rf /data/system/package_cache/*
